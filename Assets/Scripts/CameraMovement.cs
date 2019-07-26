86846ae6c6b67080e51a3c96307750d87e1029f9
Enid Medina <medina.enid@gmail.com>
2019-07-26 18:01:40 -0500

    Part 8

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -18,6 +18,8 @@ public class CameraMovement : MonoBehaviour {
   void LateUpdate() {
     if (transform.position != target.position) {
       Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+      targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+      targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
       transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
     }
   }
