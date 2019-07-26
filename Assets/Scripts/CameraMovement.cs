38c49f4ebf4319bcd09e865791f19a9a77f9bd6d
Enid Medina <medina.enid@gmail.com>
2019-07-26 09:58:01 -0500

    Assets?

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,8 @@ public class CameraMovement : MonoBehaviour {
 
   public Transform target;
   public float smoothing;
+  public Vector2 maxPosition;
+  public Vector2 minPosition;
 
   // Start is called before the first frame update
   void Start() {
